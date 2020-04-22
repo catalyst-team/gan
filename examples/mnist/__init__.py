7928dabf61b02ca9049b035e46de7b62fb33b6d8
Andrew Zharkov <andrew.zharkov@abbyy.com>
2020-04-22 19:59:35 +0300

    old mnist examples

---
+++ examples/mnist/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+import sys
+sys.path.insert(-1, '.')  # TODO: remove this terrible workaround
+from catalyst.dl import registry
+from catalyst.dl.runner import GanRunner as Runner
+from . import callbacks, models, transforms
+from .experiment import MnistGanExperiment as Experiment
+
+registry.CALLBACKS.add_from_module(callbacks)
+registry.MODELS.add_from_module(models)
